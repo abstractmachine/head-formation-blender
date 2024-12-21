83cc64bef4c41a3bc07b7a0df003d628d7f2beb2
abstractmachine <douglas@abstractmachine.net>
2024-12-21 08:02:47 +0100

    First semi-working demo

---
+++ unity/Quad/Assets/Scripts/OffsetStart.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class OffsetStart : MonoBehaviour
+{
+	// collection of all the Animators in the children
+	Animator[] animators;
+
+	void Start()
+	{
+		// get all the animators in the children
+		animators = GetComponentsInChildren<Animator>();
+
+		// wait 1 second between starting each animator
+		StartCoroutine(StartAnimators());
+
+	}
+
+	IEnumerator StartAnimators() {
+
+		// wait 1 second between starting each animator
+		float waitTime = 1.0f;
+		// go through each animator
+		foreach (Animator animator in animators) {
+			yield return new WaitForSeconds(waitTime);
+			// change a boolean parameter to start the animator
+			animator.SetBool("Started", true);
+		}
+	}
+
+}
